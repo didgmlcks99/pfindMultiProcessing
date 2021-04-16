9c6a36fe72b694b8d5b0760eafe265891950765a
didgmlcks99 <heechy06@naver.com>
2021-04-16 19:25:37 +0900

    almost final

--- pfind.c
+++ pfind.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
@@ -64,10 +65,6 @@ void term_prog(){
 	printf("Found Line : %d\n", foundLine);
 	printf("Explored Files : %d\n", expFil);
 	printf("Explored Directories : %d\n", expDir);
-
-	printf("Time (Nano) : %ld\n", time);
-	printf("Time (Micro) : %lf\n", (double)time/1000);
-	printf("Time (Milli) : %lf\n", (double)time/1000000);
 	printf("Time (Second) : %lf\n", (double)time/1000000000);
 
 	printf("Good bye.\n");
@@ -76,26 +73,15 @@ void term_prog(){
 }
 
 
-int main(int arc, char** args){
-	//start time to check
-	// struct timespec begin, end;
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	
+int main(int argc, char** args){
 #ifdef DEBUG
-	
 	printf("==> THIS IS DEBUG MODE <==\n");
 #endif
 
-	//check # of arguments 
-	int argsNum = 0;
-	do{
-		argsNum++;
-	}while(args[argsNum]);
-
 #ifdef DEBUG
-	printf("==> NUMBER OF ARGS : %d\n", argsNum);
+	printf("==> NUMBER OF ARGS : %d\n", argc);
 	printf("==> INPUT : ");
-	for(int i = 0; i < argsNum; i++){
+	for(int i = 0; i < argc; i++){
 		printf("%s ", args[i]);
 	}printf("\n");
 #endif
@@ -104,8 +90,7 @@ int main(int arc, char** args){
 	int numOpt = 0;
 	int type_path = 0;
 	int type_case = 0;
-	// int numProc = 2;
-	for(int i = 0; i < argsNum; i++){
+	for(int i = 0; i < argc; i++){
 		if(args[i][0] == '-'){
 			//handles error if user enters options not existing
 			if(args[i][1] != 'p' && args[i][1] != 'c' && args[i][1] != 'a'){
@@ -139,7 +124,7 @@ int main(int arc, char** args){
 				type_case = 1;
 			}
 		}
-	} 
+	}
 
 #ifdef DEBUG
 	printf("==> NUMBER OF OPTIONS : %d\n", numOpt);
@@ -148,14 +133,14 @@ int main(int arc, char** args){
 		printf("%s ", args[i]);
 	}printf("\n");
 	printf("==> # OF WORKERS : %d\n", numProc);
-	printf("==> PATH TYPE : %d", type_path);
-	printf("==> CASE TYPE : %d", type_case);
+	printf("==> PATH TYPE : %d\n", type_path);
+	printf("==> CASE TYPE : %d\n", type_case);
 #endif
 
 	//save space # for directory and start of keyword
 	int numDir = numOpt + 1;
 	int numKey = numDir + 1;
-	int countKey = argsNum - numKey;
+	int countKey = argc - numKey;
 
 #ifdef DEBUG
 	printf("==> DIR SPACE : %d\n", numDir);
@@ -169,14 +154,12 @@ int main(int arc, char** args){
 #endif
 
 	//handle error when input is wrong
-	if(args[numDir] == NULL || args[numKey] == NULL || argsNum == 1){
+	if(args[numDir] == NULL || args[numKey] == NULL || argc < 3){
 		printf("Error : invalid input\n");
 		printf("!Include both : directory and keyword\n");
 		printf("Terminating Program\n");
 		exit(0);
 	}
-
-//================================================================================
 
 	//make named pipe : tasks
 	if (mkfifo("tasks", 0666)) {
@@ -194,9 +177,8 @@ int main(int arc, char** args){
 		}
 	}
 
-	//make workers in the user given amount
-	// pid_t workers[numProc];
-	workers = (pid_t*)malloc(numProc*sizeof(pid_t));
+	//make workers in the user given amount with the use of malloc
+	workers = (pid_t*)malloc(numProc * sizeof(pid_t));
 	for(int i = 0; i < numProc; i++){
 		workers[i] = fork();
 		if(workers[i] == 0){
@@ -238,18 +220,6 @@ int main(int arc, char** args){
 				printf("==> WORKER[%d] > DIRECTORY '%s' IS SUCCESSFULLY OPENED\n", i, task_name);
 #endif
 
-				//open unnamed pipe for sending data from linux command, file
-				int pipes[2];
-				if(pipe(pipes) != 0){
-					perror("Error : cannot open pipe\n");
-					printf("Terminating Program\n");
-					exit(1);
-				}
-
-#ifdef DEBUG
-				printf("==> WORKER[%d] > PIPE : [%d, %d]\n", i, pipes[0], pipes[1]);
-#endif
-
 				//looks into each file of the given directory
 				struct dirent *each_file;
 				int return_stat;
@@ -266,162 +236,154 @@ int main(int arc, char** args){
 							exit(1);
 						}
 						file_mode = file_info.st_mode;
-						
-						//executes Linux command : file <file>, sending result to pipe
-						pid_t checker = fork();
-						if(checker == 0){
-							close(pipes[0]);
-							dup2(pipes[1], 1);
-							execlp("file", "file", path_name, NULL);
+
+						//gets command from file to check file type
+						FILE *fp = NULL;
+						char typed[10240] = "";
+
+						char check_file[100];
+						sprintf(check_file, "%s %s", "file", path_name);
+
+						if((fp = popen(check_file, "r")) == NULL){
+							printf("Error: cannot pipe open this file\n");
+							printf("Terminating Program\n");
 							exit(1);
-						}wait(0x0);
+						}
 
-						//differentiates the type of opened files in given task (directory)
-						pid_t maker = fork();
-						if(maker == 0){					
-							close(pipes[1]);
-							dup2(pipes[0], 0);
-
-							char typeFL[50];
-							
-							//gets file name from the pipe done the execution of Linux command : find
-							scanf("%[^\n]s", typeFL);
+						fgets(typed, 10240, fp);
 
 #ifdef DEBUG
-							printf("==> WORKER[%d] > EXEC RESULT : %s\n", i, typeFL);
+						printf("==> WORKER[%d] > EXEC RESULT : %s", i, typed);
 #endif
 
-							//flag to identify what result to send for summary
-							int flag = 0;
-							if(strstr(typeFL, "ASCII") != NULL || strstr(typeFL, "text") != NULL){
-								/*worker found a ACSII or text version file, 
-								open file explore through each line, find for matching keyword,
-								then send result regarding to 1. explored file 2. found line*/
+						//flag to identify what result to send for summary
+						int flag = 0;
+						if(strstr(typed, "ASCII") != NULL || strstr(typed, "text") != NULL){
+							/*worker found a ACSII or text version file, 
+							open file explore through each line, find for matching keyword,
+							then send result regarding to 1. explored file 2. found line*/
 #ifdef DEBUG
-								printf("WORKER[%d] > FILE TYPE : ASCII or text\n", i);
+							printf("WORKER[%d] > FILE TYPE : ASCII or text\n", i);
 #endif
-								//open detected file to be examined
-								FILE *text_file = fopen(path_name, "r");
-								if(!text_file){
-									printf("Error : cannot open this file '%s'\n", path_name);
-									continue;
+							//open detected file to be examined
+							FILE *text_file = fopen(path_name, "r");
+							if(!text_file){
+								printf("Error : cannot open this file '%s'\n", path_name);
+								continue;
+							}
+
+							//examine each line
+							char line[256];
+							int line_num = 0;
+							while(fgets(line, 256, text_file)){
+								char each_word[100][20];
+								int ctr = 0;
+								int j = 0;
+								//crop it by each word
+								for(unsigned int i = 0; i <= strlen(line); i++){
+									if(line[i] == ' ' || line[i] == '\n'){
+										each_word[ctr][j] = '\0';
+										ctr++;
+										j = 0;
+									}else{
+										if(type_case == 1){
+											each_word[ctr][j] = tolower(line[i]);
+										}else{
+											each_word[ctr][j] = line[i];
+										}
+										j++;
+									}
 								}
 
-								//examine each line
-								char line[256];
-								int line_num = 0;
-								while(fgets(line, 256, text_file)){
-									char each_word[100][20];
-									int ctr = 0;
-									int j = 0;
-									//crop it by each word
-									for(int i = 0; i <= strlen(line); i++){
-										if(line[i] == ' ' || line[i] == '\n'){
-											each_word[ctr][j] = '\0';
-											ctr++;
-											j = 0;
-										}else{
-											if(type_case == 1){
-												each_word[ctr][j] = tolower(line[i]);
-											}else{
-												each_word[ctr][j] = line[i];
-											}
-											j++;
-										}
-									}
-
-									if(type_case == 1){
-										for(int i = numKey; i < numKey + countKey; i++){
-											for(int j = 0; j <= strlen(args[numKey]); j++){
-												args[i][j] = tolower(args[i][j]);
-											}
-										}
-									}
-									
-									//compare each word from file with the keywords chosen by user,
-									int correct;
+								if(type_case == 1){
 									for(int i = numKey; i < numKey + countKey; i++){
-										correct = 0;
-										for(int j = 0; j < ctr; j++){
-											if(strcmp(each_word[j], args[i]) == 0){
-												correct = 1;
-												break;
-											}
+										for(unsigned int j = 0; j <= strlen(args[numKey]); j++){
+											args[i][j] = tolower(args[i][j]);
 										}
-										if(correct == 0){
+									}
+								}
+								
+								//compare each word from file with the keywords chosen by user,
+								int correct;
+								for(int i = numKey; i < numKey + countKey; i++){
+									correct = 0;
+									for(int j = 0; j < ctr; j++){
+										if(strcmp(each_word[j], args[i]) == 0){
+											correct = 1;
 											break;
 										}
 									}
+									if(correct == 0){
+										break;
+									}
+								}
 
-									//if and only if, all the keywords are found than the line will be printed
-									if(correct == 1){
-										//print result regarding to type of path
-										if(type_path == 1){
-											printf("%s : %d : %s", realpath(path_name, NULL), line_num, line);
-										}else{
-											printf("%s : %d : %s", path_name, line_num, line);
-										}
+								//if and only if, all the keywords are found than the line will be printed
+								if(correct == 1){
+									//print result regarding to type of path
+									if(type_path == 1){
+										printf("%s : %d : %s", realpath(path_name, NULL), line_num, line);
+									}else{
+										printf("%s : %d : %s", path_name, line_num, line);
+									}
 
-										size_t cor_len = strlen("foundLine");
-										flock(results_send, LOCK_EX);
-										//send the line_cor name for each 'foundLine' to the named pipe, results
-										if(write_bytes(results_send, &cor_len, sizeof(cor_len)) != sizeof(cor_len)){}
-										if(write_bytes(results_send, "foundLine", cor_len) != cor_len){
-											flock(results_send, LOCK_UN);
-											break;
-										}
+									size_t cor_len = strlen("foundLine");
+									flock(results_send, LOCK_EX);
+									//send the line_cor name for each 'foundLine' to the named pipe, results
+									if(write_bytes(results_send, &cor_len, sizeof(cor_len)) != sizeof(cor_len)){}
+									if((unsigned int)write_bytes(results_send, "foundLine", cor_len) != cor_len){
 										flock(results_send, LOCK_UN);
+										break;
 									}
-									line_num++;
-								}
-								flag = 1;
-							}else if(S_ISDIR(file_mode) && strstr(typeFL, "directory") != NULL){
-								/*worker found a directory,
-								send to manager as a new taskv through named pipe, results, 
-								then send result regarding to 3. explored directory*/
-#ifdef DEBUG
-								printf("==> WORKER[%d] > FILE TYPE : directory\n", i);
-								printf("==> WORKER[%d] > SENT TASK : %s\n", i, path_name);
-#endif
-								size_t len = strlen(path_name);
-								flock(results_send, LOCK_EX);
-								//send the full path name for the found directory to the named pipe, results
-								if(write_bytes(results_send, &len, sizeof(len)) != sizeof(len)){}
-								if(write_bytes(results_send, path_name, len) != len){
 									flock(results_send, LOCK_UN);
-									break;
 								}
-								flock(results_send, LOCK_UN);
-
-								flag = 2;
-							}else{
+								line_num++;
+							}
+							flag = 1;
+						}else if(S_ISDIR(file_mode) && strstr(typed, "directory") != NULL){
+							/*worker found a directory,
+							send to manager as a new taskv through named pipe, results, 
+							then send result regarding to 3. explored directory*/
 #ifdef DEBUG
-								printf("FILE TYPE : not regular\n");
+							printf("==> WORKER[%d] > FILE TYPE : directory\n", i);
+							printf("==> WORKER[%d] > SENT TASK : %s\n", i, path_name);
 #endif
-								flag = 0;
-							}
-
-							if(flag != 0){
-								char result_info[20];
-
-								if(flag == 1) strcpy(result_info, "expFil");
-								if(flag == 2) strcpy(result_info, "expDir");
-
-								size_t result_len = strlen(result_info);
-								flock(results_send, LOCK_EX);
-								//send the result_info name for each 'expFil', 'expDir' to the named pipe, results
-								if(write_bytes(results_send, &result_len, sizeof(result_len)) != sizeof(result_len)){}
-								if(write_bytes(results_send, result_info, result_len) != result_len){
-									flock(results_send, LOCK_UN);
-									break;
-								}
+							size_t len = strlen(path_name);
+							flock(results_send, LOCK_EX);
+							//send the full path name for the found directory to the named pipe, results
+							if(write_bytes(results_send, &len, sizeof(len)) != sizeof(len)){}
+							if((unsigned int)write_bytes(results_send, path_name, len) != len){
 								flock(results_send, LOCK_UN);
-
-								flag = 0;
+								break;
 							}
+							flock(results_send, LOCK_UN);
 
-							exit(1);
-						}wait(0x0);
+							flag = 2;
+						}else{
+#ifdef DEBUG
+							printf("FILE TYPE : not regular\n");
+#endif
+							flag = 0;
+						}
+
+						if(flag != 0){
+							char result_info[20];
+
+							if(flag == 1) strcpy(result_info, "expFil");
+							if(flag == 2) strcpy(result_info, "expDir");
+
+							size_t result_len = strlen(result_info);
+							flock(results_send, LOCK_EX);
+							//send the result_info name for each 'expFil', 'expDir' to the named pipe, results
+							if(write_bytes(results_send, &result_len, sizeof(result_len)) != sizeof(result_len)){}
+							if((unsigned int)write_bytes(results_send, result_info, result_len) != result_len){
+								flock(results_send, LOCK_UN);
+								break;
+							}
+							flock(results_send, LOCK_UN);
+
+							flag = 0;
+						}
 					}
 				}
 			}
@@ -431,6 +393,9 @@ int main(int arc, char** args){
 		}
 	}
 
+	//start time to check
+	clock_gettime(CLOCK_MONOTONIC, &begin);
+
 	char task[128];
 	//first task will be initiated with the directory user have chosen
 	strcpy(task, args[numDir]);
@@ -439,10 +404,6 @@ int main(int arc, char** args){
 
 	int tasks_send = open("tasks", O_WRONLY | O_SYNC);
 	int results_rec = open("results", O_RDONLY | O_SYNC);
-
-	// int foundLine = 0;
-	// int expFil = 0;
-	// int expDir = 1;
 
 	//recognize termination signal from user
 	signal(SIGINT, term_prog);
@@ -455,7 +416,7 @@ int main(int arc, char** args){
 		size_t len = strlen(task);
 		//manager send task through named pipe tasks
 		if(write_bytes(tasks_send, &len, sizeof(len)) != sizeof(len)){}
-		if(write_bytes(tasks_send, task, len) != len){}
+		if((unsigned int)write_bytes(tasks_send, task, len) != len){}
 			
 		while(1){
 			size_t length, bs;
